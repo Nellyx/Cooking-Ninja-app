eafc0c9a3c7e5f268632b11155065cc7a4cd8a28
Nellyx <neilsonmike14@gmail.com>
2022-06-04 02:11:45 -0700

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,8 @@ function Navbar() {
   return (
     <div className="navbar">
       <nav>
-        <Link to="/">
+        <Link to="/" 
+           className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
         <Link to="/create">Create Recipe</Link>
