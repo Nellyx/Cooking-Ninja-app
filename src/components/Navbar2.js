212cc260f81d97b9a6dae0a956aecc69155a4fea
Nellyx <neilsonmike14@gmail.com>
2022-06-04 04:39:06 -0700

    navbar seetup completed trial phase

---
+++ src/components/Navbar2.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar2() {
+  return (
+    <div className="navbar2">
+      <nav>
+
+Link
+
+
+
+
+	  </nav>
+    </div>
+  );
+}
+
+export default Navbar2;
